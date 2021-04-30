57047fb40eea677f6fe41bb17105ddf159203330
StefanoCeli <stefanopsn91@gmail.com>
2021-04-30 21:59:08 +0200

    fine seconda parte

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    
+    //array di gatti
     const gatti = [
         {
              nome : 'Chanel', 
@@ -50,17 +50,59 @@ $(document).ready(function(){
              sesso : 'maschio'      
         }
     ]
-    gatti.forEach(gatto => $('#milestone-1 ul').append(stampaLista(gatto)));
+
+   //uso forEach er stampare a video ogni singolo oggetto
+    gatti.forEach(gatto => $('#milestone-1 ul').append(stampaLista(gatto.colore,gatto.nome)));
+
+    //creo un nuovo array di gatti che conterrà il fiocco
+    const newGatti= gatti.map(gatto=>{
+         let rosa= 'rgb(243, 111, 214)';
+         let blu= 'rgb(0, 110, 255)';
+         let genderFiocco = (gatto.sesso === 'femmina') ? rosa : blu;
+         let opacity = gatto.eta /10;
+         
+         return {
+               ...gatto,
+               fiocco:
+                    {
+                         colore : genderFiocco,
+                         opacity :  opacity
+                    }
+               };
+     })
+     console.log(newGatti);
+
+     //vado a creare 2 array dove uno sarà di gatti gemmina e l'altro di gatti maschi usando il filter
+     const gattiMaschi = newGatti.filter(gatto => gatto.sesso === 'maschio');
+     const gattiFemmine = newGatti.filter(gatto => gatto.sesso === 'femmina');
+     //console.log dei nuovi array
+     console.log(gattiMaschi);
+     console.log(gattiFemmine);
+
+     //forEach per stamparli
+     gattiMaschi.forEach(gatto => $('#milestone-2 .maschi ul').append(stampaLista(gatto.colore,gatto.nome,gatto.fiocco.colore,gatto.fiocco.opacity)));
+     gattiFemmine.forEach(gatto => $('#milestone-2 .femmine ul').append(stampaLista(gatto.colore,gatto.nome,gatto.fiocco.colore,gatto.fiocco.opacity)));
+
+     //unisco i due array in un unico array e lo destrutturo
    
 });
-
-const stampaLista= (gatto)=>{
-     const html =`
+//funzione per stampare a video 
+const stampaLista = (colore,nome,...fiocco)=>{
+     //dichiaro una variabile stringa vuota
+     let fioccoStr='';
+     //condizione per fiocco,se avrà degli elementi all'interno allora la stringa prenderà un nuovo valore
+     if(fiocco.length > 0){
+          fioccoStr=`
+                    <i class="fas fa-ribbon" style= "color:${fiocco[0]};
+                                             opacity:${fiocco[1]}";></i>
+          `
+     }
+     const html= `
                <li>
-                    <i class="fas fa-cat" style="color:${gatto.colore};"></i>
-                    <span>${gatto.nome}</span>
+                    <i class="fas fa-cat" style="color:${colore}";></i>
+                    ${fioccoStr}
+                    <span>${nome}</span>
                 </li>
      `
-     return html
-}
-
+     return html;
+};
